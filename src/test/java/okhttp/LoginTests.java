1fe650da3047ff16e27d93a8cbbbaa94c47024fc
Alina-QA-Engineer <alinaqatests@gmail.com>
2025-01-07 12:40:11 +0200

    Created LoginTests

---
+++ src/test/java/okhttp/LoginTests.java
@@ -0,0 +1,171 @@
+package okhttp;
+
+import dto.UserDTO;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.BaseAPI;
+
+import java.io.IOException;
+
+public class LoginTests implements BaseAPI {
+
+    @Test
+    public void login() {
+
+        UserDTO user = UserDTO.builder().username("testemail@example.com").password("Password123!").build();
+
+        RequestBody requestBody = RequestBody.create(GSON.toJson(user), JSON);
+        Request request = new Request.Builder().url(BASE_URL + LOGIN).post(requestBody).build();
+        Response response;
+        try {
+            response = OK_HTTP_CLIENT.newCall(request).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(response.isSuccessful());
+        System.out.println(response.toString());
+        try {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assert.assertEquals(response.code(), 200);
+
+    }
+
+    @Test
+    public void login_401_emptyUsername() {
+
+        UserDTO user = UserDTO.builder().username("").password("Password123!").build();
+
+        RequestBody requestBody = RequestBody.create(GSON.toJson(user), JSON);
+        Request request = new Request.Builder().url(BASE_URL + LOGIN).post(requestBody).build();
+        Response response;
+        try {
+            response = OK_HTTP_CLIENT.newCall(request).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(response.isSuccessful());
+        System.out.println(response.toString());
+        try {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assert.assertEquals(response.code(), 401);
+
+    }
+
+    @Test
+    public void login_401_emptyPassword() {
+
+        UserDTO user = UserDTO.builder().username("testemail@example.com").password("").build();
+
+        RequestBody requestBody = RequestBody.create(GSON.toJson(user), JSON);
+        Request request = new Request.Builder().url(BASE_URL + LOGIN).post(requestBody).build();
+        Response response;
+        try {
+            response = OK_HTTP_CLIENT.newCall(request).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(response.isSuccessful());
+        System.out.println(response.toString());
+        try {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assert.assertEquals(response.code(), 401);
+
+    }
+
+    @Test
+    public void login_401_invalidUsername() {
+
+        UserDTO user = UserDTO.builder().username("testemailexample.com").password("Password123!").build();
+
+        RequestBody requestBody = RequestBody.create(GSON.toJson(user), JSON);
+        Request request = new Request.Builder().url(BASE_URL + LOGIN).post(requestBody).build();
+        Response response;
+        try {
+            response = OK_HTTP_CLIENT.newCall(request).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(response.isSuccessful());
+        System.out.println(response.toString());
+        try {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assert.assertEquals(response.code(), 401);
+
+    }
+
+    @Test
+    public void login_401_invalidPassword() {
+
+        UserDTO user = UserDTO.builder().username("testemail@example.com").password("Password123").build();
+
+        RequestBody requestBody = RequestBody.create(GSON.toJson(user), JSON);
+        Request request = new Request.Builder().url(BASE_URL + LOGIN).post(requestBody).build();
+        Response response;
+        try {
+            response = OK_HTTP_CLIENT.newCall(request).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(response.isSuccessful());
+        System.out.println(response.toString());
+        try {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assert.assertEquals(response.code(), 401);
+
+    }
+
+    @Test
+    public void login_401_unregisteredUser() {
+
+        UserDTO user = UserDTO.builder().username("newemailtesttest@example.com").password("Password123!").build();
+
+        RequestBody requestBody = RequestBody.create(GSON.toJson(user), JSON);
+        Request request = new Request.Builder().url(BASE_URL + LOGIN).post(requestBody).build();
+        Response response;
+        try {
+            response = OK_HTTP_CLIENT.newCall(request).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(response.isSuccessful());
+        System.out.println(response.toString());
+        try {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assert.assertEquals(response.code(), 401);
+
+    }
+
+}
