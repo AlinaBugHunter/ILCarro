680d851fc6b3e1ba79d2bd61b2236853a64e35ad
Alina-QA-Engineer <alinaqatests@gmail.com>
2025-01-14 16:07:26 +0200

    Add new tests to GetUserCarsTests

--- src/test/java/okhttp/GetUserCarsTests.java
+++ src/test/java/okhttp/GetUserCarsTests.java
@@ -1,8 +1,6 @@
 package okhttp;
 
-import dto.CarsDTO;
-import dto.TokenDTO;
-import dto.UserDTO;
+import dto.*;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
@@ -23,12 +21,15 @@ public class GetUserCarsTests implements BaseAPI {
 
     @BeforeClass
     public void login() {
+
         UserDTO user = UserDTO.builder()
                 .username(getProperty("login.properties", "username"))
                 .password(getProperty("login.properties", "password"))
                 .build();
+
         RequestBody requestBody = RequestBody.create(GSON.toJson(user), JSON);
         Request request = new Request.Builder().url(BASE_URL + LOGIN).post(requestBody).build();
+
         try (Response response = OK_HTTP_CLIENT.newCall(request).execute()) {
             if (response.isSuccessful()) {
                 tokenDTO = GSON.fromJson(response.body().string(), TokenDTO.class);
@@ -39,7 +40,10 @@ public class GetUserCarsTests implements BaseAPI {
         } catch (IOException e) {
             System.out.println("Created Exception -> login()");
         }
+
     }
+
+    // Status Code: 200
 
     @Test
     public void getUserCars() {
@@ -50,8 +54,7 @@ public class GetUserCarsTests implements BaseAPI {
                 .get()
                 .build();
 
-        try {
-            Response response = OK_HTTP_CLIENT.newCall(request).execute();
+        try (Response response = OK_HTTP_CLIENT.newCall(request).execute()) {
             if (response.isSuccessful()) {
                 softAssert.assertEquals(response.code(), 200);
                 CarsDTO carsDTO = GSON.fromJson(response.body().string(), CarsDTO.class);
@@ -62,6 +65,109 @@ public class GetUserCarsTests implements BaseAPI {
             }
         } catch (IOException e) {
             Assert.fail("Created Exception -> getUserCars()");
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    // Status Code: 400
+
+    @Test
+    public void getUserCars_400_invalidRequestMethod() {
+
+        Request request = new Request.Builder()
+                .url(BASE_URL + GET_USER_CARS)
+                .addHeader(AUTH, tokenDTO.getAccessToken())
+                .delete()
+                .build();
+
+        try (Response response = OK_HTTP_CLIENT.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                ErrorMessageDTO errorMessageDTO = GSON.fromJson(response.body().string(), ErrorMessageDTO.class);
+                softAssert.assertEquals(response.code(), 400);
+                softAssert.assertEquals(errorMessageDTO.getError(), "Bad Request");
+                softAssert.assertTrue(errorMessageDTO.getMessage().equals("Car with serial number my not found"));
+                softAssert.assertAll();
+            } else {
+                Assert.fail("Response Status Code -> " + response.code());
+            }
+        } catch (IOException e) {
+            Assert.fail("Created Exception -> getUserCars_400_invalidRequestMethod()");
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    // Status Code: 401
+
+    @Test
+    public void getUserCars_401_Unauthorized() {
+
+        Request request = new Request.Builder()
+                .url(BASE_URL + GET_USER_CARS)
+                .addHeader(AUTH, tokenDTO.getAccessToken() + "invalidToken")
+                .get()
+                .build();
+
+        try (Response response = OK_HTTP_CLIENT.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                ErrorMessageDTO errorMessageDTO = GSON.fromJson(response.body().string(), ErrorMessageDTO.class);
+                softAssert.assertEquals(response.code(), 401);
+                softAssert.assertEquals(errorMessageDTO.getError(), "Unauthorized");
+                softAssert.assertTrue(errorMessageDTO.getMessage().equals("JWT signature does not match locally computed " +
+                        "signature. JWT validity cannot be asserted and should not be trusted."));
+                softAssert.assertAll();
+            } else {
+                Assert.fail("Response Status Code -> " + response.code());
+            }
+        } catch (IOException e) {
+            Assert.fail("Created Exception -> getUserCars_401_Unauthorized()");
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    // Status Code: 403
+
+    @Test
+    public void getUserCars_403_invalidURL() {
+
+        Request request = new Request.Builder()
+                .url(BASE_URL + GET_USER_CARS + "/invalidURL")
+                .addHeader(AUTH, tokenDTO.getAccessToken())
+                .get()
+                .build();
+
+        try (Response response = OK_HTTP_CLIENT.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                Assert.assertEquals(response.code(), 403);
+            } else {
+                Assert.fail("Response Status Code -> " + response.code());
+            }
+        } catch (IOException e) {
+            Assert.fail("Created Exception -> getUserCars_403_invalidURL()");
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void getUserCars_403_WOHeaders() {
+
+        Request request = new Request.Builder()
+                .url(BASE_URL + GET_USER_CARS)
+                .get()
+                .build();
+
+        try (Response response = OK_HTTP_CLIENT.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                Assert.assertEquals(response.code(), 403);
+            } else {
+                Assert.fail("Response Status Code -> " + response.code());
+            }
+        } catch (IOException e) {
+            Assert.fail("Created Exception -> getUserCars_403_WOHeaders()");
+            System.out.println(e.getMessage());
         }
 
     }
